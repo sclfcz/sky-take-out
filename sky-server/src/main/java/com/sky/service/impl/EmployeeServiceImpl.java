9bc0ac01ee67eb6870864f96a859098cb10fe3ca
hepushuo <2775885364@qq.com>
2026-03-10 10:59:11 +0800

    启用禁用员工账号业务代码开发

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -111,4 +111,25 @@ public class EmployeeServiceImpl implements EmployeeService {
         return new PageResult(total,records);
     }
 
+    /**
+     * 启用禁用员工账号
+     * @param status
+     * @param id
+     */
+    @Override
+    public void startOrStop(Integer status, Long id) {
+//        Employee employee = new Employee();
+//        employee.setStatus(status);
+//        employee.setId(id);
+
+        //使用构建器模式@Builder设置要更新的字段
+        Employee employee = Employee.builder()
+                .status(status)
+                .id(id)
+                .build();
+
+        //通过动态语句修改，可用性更高
+        employeeMapper.update(employee);
+    }
+
 }
