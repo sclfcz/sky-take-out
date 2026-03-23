31332c5c0bb2b27204fb5c24ba7ad206d5da9ad8
hepushuo <2775885364@qq.com>
2026-03-23 21:22:36 +0800

    店铺营业状态设置业务代码开发

---
+++ sky-server/src/main/java/com/sky/controller/admin/ShopController.java
@@ -0,0 +1,51 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.web.bind.annotation.*;
+
+@RestController("adminShopController")
+@RequestMapping("/admin/shop")
+@Api(tags = "商家信息相关接口")
+@Slf4j
+public class ShopController {
+
+    private static final String KEY = "SHOP_STATUS";
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    /**
+     * 设置店铺的营业状态
+     * @param status
+     * @return
+     */
+    @PutMapping("/{status}")
+    @ApiOperation("设置店铺的营业状态")
+    public Result setStatus(@PathVariable Integer status){
+        log.info("设置店铺的营业状态为：{}", status==1?"营业中":"打烊中");
+        redisTemplate.opsForValue().set(KEY, status);
+        return Result.success();
+    }
+
+    /**
+     * 获取店铺的营业状态
+     * @return
+     */
+    @GetMapping("/status")
+    @ApiOperation("获取店铺的营业状态")
+    public Result<Integer> getStatus(){
+        Integer shopStatus = (Integer) redisTemplate.opsForValue().get(KEY);
+        // 如果 Redis 中没有缓存店铺状态，默认返回营业中 (1)
+        if (shopStatus == null) {
+            shopStatus = 1;
+        }
+        log.info("获取店铺的营业状态为：{}", shopStatus==1?"营业中":"打烊中");
+        return Result.success(shopStatus);
+    }
+
+}
