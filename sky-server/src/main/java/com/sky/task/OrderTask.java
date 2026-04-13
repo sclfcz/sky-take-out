b014e4eff16acca1b6dd8f9c399d3b32e71b180f
hepushuo <2775885364@qq.com>
2026-04-13 19:24:59 +0800

    订单状态定时处理代码开发

---
+++ sky-server/src/main/java/com/sky/task/OrderTask.java
@@ -0,0 +1,60 @@
+package com.sky.task;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+/**
+ * 定时任务类,定时处理订单状态
+ */
+@Component
+@Slf4j
+public class OrderTask {
+
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 定时处理超时订单
+     */
+    @Scheduled(cron = "0 * * * * ? ")//每分钟触发一次
+    public void processTimeoutOrder(){
+        log.info(" 定时处理超时订单：{}", LocalDateTime.now());
+
+        //select * from orders where status = ？and order_time < (当前时间-15)
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.PENDING_PAYMENT, LocalDateTime.now().minusMinutes(15));
+
+        if (ordersList != null && ordersList.size() > 0) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.CANCELLED);
+                orders.setCancelTime(LocalDateTime.now());
+                orders.setCancelReason("订单超时未支付，系统自动取消");
+
+                orderMapper.update(orders);
+            }
+        }
+
+    }
+
+    /**
+     * 定时处理派送中接单
+     */
+    @Scheduled(cron = "0 0 1 * * ? ")//每天凌晨一点
+    public void processDeliveryOrder() {
+        log.info(" 定时处理派送中接单：{}", LocalDateTime.now());
+
+        List<Orders> ordersList = orderMapper.getByStatusAndOrderTimeLT(Orders.DELIVERY_IN_PROGRESS, LocalDateTime.now().minusMinutes(60));
+        if (ordersList != null && ordersList.size() > 0) {
+            for (Orders orders : ordersList) {
+                orders.setStatus(Orders.COMPLETED);
+                orderMapper.update(orders);
+            }
+        }
+    }
+}
